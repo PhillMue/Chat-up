de4cf72399bb4f7c3c193e4675b3d330d0e0b1b0
PhillMue <muenzuve@gmail.com>
2016-03-20 23:30:28 +0300

    completed adapter

--- app/src/main/java/mue/com/chatup/ChatAdapter.java
+++ app/src/main/java/mue/com/chatup/ChatAdapter.java
@@ -2,7 +2,9 @@ package mue.com.chatup;
 
 import android.content.Context;
 import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
@@ -26,4 +28,53 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             //mCardView = (CardView) itemView.findViewById(R.id.card_view);
         }
     }
+    public ChatAdapter(Context context, List<Chat> myDataset, String id) {
+        mContext = context;
+        mDataset = myDataset;
+        selectedItems = new SparseBooleanArray();
+        mId = id;
+    }
+
+    @Override
+    public ChatAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v;
+
+        if(viewType == 1)
+        {
+            v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_chat_right, parent, false);
+        }
+        else
+        {
+            v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_chat_left, parent, false);
+        }
+
+        ViewHolder vh = new ViewHolder(v);
+        Integer i = vh.getPosition();
+        v.setTag(vh);
+
+        return vh;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(mDataset.get(position).getId().equals(mId))
+            return 1;
+
+        return 2;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Chat chat = mDataset.get(position);
+        holder.mTextView.setText(chat.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
 }
+
+
