c722cfb5f0621638a645625da53fdf9a4062fe91
PhillMue <muenzuve@gmail.com>
2016-03-28 20:07:27 +0300

    system files

---
+++ app/src/androidTest/java/mue/com/chatup/ChatAdapterTest.java
@@ -0,0 +1,25 @@
+package mue.com.chatup;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by mue on 3/28/16.
+ */
+public class ChatAdapterTest extends TestCase {
+
+    public void testOnCreateViewHolder() throws Exception {
+
+    }
+
+    public void testGetItemViewType() throws Exception {
+
+    }
+
+    public void testOnBindViewHolder() throws Exception {
+
+    }
+
+    public void testGetItemCount() throws Exception {
+
+    }
+}
